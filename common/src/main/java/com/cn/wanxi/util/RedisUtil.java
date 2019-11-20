c8de63e66632698947da1b5508644a0c956f4b5c
neolyao <neolyao@sina.com>
2019-11-20 21:39:52 +0800

    添加redis和发短信功能

---
+++ common/src/main/java/com/cn/wanxi/util/RedisUtil.java
@@ -0,0 +1,29 @@
+package com.cn.wanxi.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @program: tenmallfront
+ * @description: redis 缓存工具类
+ * @author: niyao
+ * @create: 2019-11-20 19:25
+ */
+@Component
+public class RedisUtil {
+    @Autowired
+    private static  RedisTemplate redisTemplate;
+    public  static  boolean isCodeExist(String phone,String code){
+        ValueOperations<String,String> ops=redisTemplate.opsForValue();
+        return  ops.get(phone).equals(code);
+    }
+    public static void setCode(String phone,String code){
+        ValueOperations<String,String> ops=redisTemplate.opsForValue();
+        ops.set(phone,code);
+        redisTemplate.expire(phone,1, TimeUnit.MINUTES);//过期时间1分钟
+    }
+}
