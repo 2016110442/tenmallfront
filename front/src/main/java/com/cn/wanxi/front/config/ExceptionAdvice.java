cb95d51009549c0e835851d71562430f56c6e12e
neolyao <neolyao@sina.com>
2019-11-27 16:05:03 +0800

    Merge branch 'master' of http://github.com/2016110442/tenmallfront
    # Conflicts:
    #	common/target/classes/com/cn/wanxi/util/RedisUtil.class
    #	common/target/classes/com/cn/wanxi/util/SendMessageUtil.class
    #	service/target/classes/com/cn/wanxi/service/user/UserServiceImpl.class
    #	service/target/classes/com/cn/wanxi/service/user/WxTabReturnCauseServiceImpl.class

---
+++ front/src/main/java/com/cn/wanxi/front/config/ExceptionAdvice.java
@@ -0,0 +1,49 @@
+package com.cn.wanxi.front.config;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.*;
+
+/**
+ * @program: tenmallfront
+ * @description:异常捕获
+ * @author: niyao
+ * @create: 2019-11-27 14:42
+ */
+@ControllerAdvice
+@RestController
+public class ExceptionAdvice {
+    /**
+     * 拦截捕捉自定义异常 ConstraintViolationException.class
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public Map ConstraintViolationExceptionHandler(ConstraintViolationException ex) {
+        Map map = new HashMap();
+
+        Set<ConstraintViolation<?>> constraintViolations = ex.getConstraintViolations();
+
+        Iterator<ConstraintViolation<?>> iterator = constraintViolations.iterator();
+        //存放异常消息
+        List<String> msgList = new ArrayList<>();
+        //
+        while (iterator.hasNext()) {
+            ConstraintViolation<?> cvl = iterator.next();
+            msgList.add(cvl.getMessageTemplate());
+        }
+        map.put("msg",msgList);
+        return map;
+    }
+    @ExceptionHandler(value = NumberFormatException.class)
+    public Map numberExceptionHandler(){
+        Map map = new HashMap();
+        map.put("msg","傻吊int型的参数传你妹的字符串啊");
+        return map;
+    }
+
+}
