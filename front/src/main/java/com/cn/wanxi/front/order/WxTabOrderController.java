0805a9218fc940c352929d4a332d0e510e7a3d84
wyy <wyy@example.com>
2019-11-28 10:05:04 +0800

    修改search和插入订单

--- front/src/main/java/com/cn/wanxi/front/order/WxTabOrderController.java
+++ front/src/main/java/com/cn/wanxi/front/order/WxTabOrderController.java
@@ -1,6 +1,5 @@
 package com.cn.wanxi.front.order;
 
-import com.cn.wanxi.model.advertisin.Result;
 import com.cn.wanxi.model.order.WxOrderVO;
 import com.cn.wanxi.service.order.WxTabOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author
@@ -21,15 +23,20 @@ public class WxTabOrderController {
     private WxTabOrderService wxTabOrderService;
 
     @RequestMapping(value = "/submit",method = RequestMethod.POST)
-    public Result insert(@RequestBody WxOrderVO wxOrderVO){
-        Result result = new Result();
-        int i = wxTabOrderService.insert(wxOrderVO);
-        if (i==1){
-            result.setCode(1);
-            result.setMessage("XXX");
-        }else{
-            result.setCode(0);
-            result.setMessage("XXX");
+    public Map<String,String> insert(@RequestBody(required = false) WxOrderVO wxOrderVO){
+        Map<String,String> result = new HashMap<>();
+        if (wxOrderVO==null){
+            result.put("code","1");
+            result.put("payUrl","你想干哈");
+        }else {
+            int i = wxTabOrderService.insert(wxOrderVO);
+            if (i==1){
+                result.put("code","0");
+                result.put("payUrl","http://alipay.com/pay.jpg");
+            }else{
+                result.put("code","1");
+                result.put("message","对不起，有必填项为空了");
+            }
         }
         return result;
     }
---
+++ model/src/main/java/com/cn/wanxi/model/productdetails/ProductSearch.java
@@ -0,0 +1,142 @@
+package com.cn.wanxi.model.productdetails;
+
+import com.cn.wanxi.model.cart.WxTabCart;
+import com.cn.wanxi.model.cart.WxTabSku;
+
+public class ProductSearch {
+    private String name;
+    private String caption;
+    private Integer brandId;
+    private Integer category1Id;
+    private Integer category2Id;
+    private Integer category3Id;
+    private Integer freightId;
+    private String image;
+    private String images;
+    private String introduction;
+    private String paraItems;
+    private String saleService;
+    private String sn;
+    private String specItems;
+    private Integer templateId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public Integer getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Integer brandId) {
+        this.brandId = brandId;
+    }
+
+    public Integer getCategory1Id() {
+        return category1Id;
+    }
+
+    public void setCategory1Id(Integer category1Id) {
+        this.category1Id = category1Id;
+    }
+
+    public Integer getCategory2Id() {
+        return category2Id;
+    }
+
+    public void setCategory2Id(Integer category2Id) {
+        this.category2Id = category2Id;
+    }
+
+    public Integer getCategory3Id() {
+        return category3Id;
+    }
+
+    public void setCategory3Id(Integer category3Id) {
+        this.category3Id = category3Id;
+    }
+
+    public Integer getFreightId() {
+        return freightId;
+    }
+
+    public void setFreightId(Integer freightId) {
+        this.freightId = freightId;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getImages() {
+        return images;
+    }
+
+    public void setImages(String images) {
+        this.images = images;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public String getParaItems() {
+        return paraItems;
+    }
+
+    public void setParaItems(String paraItems) {
+        this.paraItems = paraItems;
+    }
+
+    public String getSaleService() {
+        return saleService;
+    }
+
+    public void setSaleService(String saleService) {
+        this.saleService = saleService;
+    }
+
+    public String getSn() {
+        return sn;
+    }
+
+    public void setSn(String sn) {
+        this.sn = sn;
+    }
+
+    public String getSpecItems() {
+        return specItems;
+    }
+
+    public void setSpecItems(String specItems) {
+        this.specItems = specItems;
+    }
+
+    public Integer getTemplateId() {
+        return templateId;
+    }
+
+    public void setTemplateId(Integer templateId) {
+        this.templateId = templateId;
+    }
+}
