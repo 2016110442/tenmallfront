333894e4bf2026d5e79004efd75d140e40d32db2
neolyao <neolyao@sina.com>
2019-12-09 16:35:40 +0800

    购物车 修复

--- front/src/main/java/com/cn/wanxi/front/FrontApplication.java
+++ front/src/main/java/com/cn/wanxi/front/FrontApplication.java
@@ -1,8 +1,8 @@
 package com.cn.wanxi.front;
 
-		import org.mybatis.spring.annotation.MapperScan;
-		import org.springframework.boot.SpringApplication;
-		import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.cn.wanxi"})
 @MapperScan("com.cn.wanxi.dao")
