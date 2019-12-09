5b77c0843836ad5cdc36ab43d9ab5df5ad333afb
Jasmiar <jasminershare@sina.com>
2019-12-09 10:55:55 +0800

    修改POM配置

--- front/src/test/java/com/cn/wanxi/front/FrontApplicationTests.java
+++ front/src/test/java/com/cn/wanxi/front/FrontApplicationTests.java
@@ -3,10 +3,10 @@ package com.cn.wanxi.front;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class FrontApplicationTests {
 
-    @Test
+
     void contextLoads() {
     }
 
