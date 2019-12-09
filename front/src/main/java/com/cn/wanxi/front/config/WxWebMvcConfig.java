a006119b0ec8d159e4d03eb85fb8fc1296d9a958
neolyao <neolyao@sina.com>
2019-12-09 17:03:36 +0800

    购物车 修复

--- front/src/main/java/com/cn/wanxi/front/config/WxWebMvcConfig.java
+++ front/src/main/java/com/cn/wanxi/front/config/WxWebMvcConfig.java
@@ -1,14 +1,12 @@
 package com.cn.wanxi.front.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
